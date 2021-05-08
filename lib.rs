11c71fbac58a7d72d24d57af3e95e7574b7a9212
Muhammad Anjum Kaiser <anjumbutt@gmail.com>
2021-05-09 03:56:22 +0500

    cargo fmt to indent code properly

--- lib.rs
+++ lib.rs
@@ -1,8 +1,7 @@
 #![cfg_attr(not(feature = "std"), no_std)]
 
 use ink_lang as ink;
-use ink_storage::collections::{Vec, HashMap, Stash, Bitvec};
-
+use ink_storage::collections::{Bitvec, HashMap, Stash, Vec};
 
 #[ink::contract]
 mod incrementer {
@@ -21,7 +20,6 @@ mod incrementer {
         // store a number using lazy
         lazy_number: ink_storage::Lazy<u32>,
 
-
         // store a mapping from AccountId to u32
         account_number_map: ink_storage::collections::HashMap<AccountId, u32>,
 
@@ -32,16 +30,22 @@ mod incrementer {
     impl Incrementer {
         /// Constructor that initializes the `bool` value to the given `init_value`.
         #[ink(constructor)]
-        pub fn new(init_value: bool, init_number: u32, init_lazy_number: u32, init_balance: Balance) -> Self {
-            let init_account_number_map: ink_storage::collections::HashMap<AccountId, u32> = ink_storage::collections::HashMap::new();
+        pub fn new(
+            init_value: bool,
+            init_number: u32,
+            init_lazy_number: u32,
+            init_balance: Balance,
+        ) -> Self {
+            let init_account_number_map: ink_storage::collections::HashMap<AccountId, u32> =
+                ink_storage::collections::HashMap::new();
 
             Self {
                 bool_value: init_value,
                 number: init_number,
-                lazy_number: ink_storage::Lazy::<u32>::new(init_lazy_number), 
-                account_number_map:  init_account_number_map,
+                lazy_number: ink_storage::Lazy::<u32>::new(init_lazy_number),
+                account_number_map: init_account_number_map,
                 balance: init_balance,
-             }
+            }
         }
 
         /// Constructor that initializes the `bool` value to `false`.
@@ -49,8 +53,12 @@ mod incrementer {
         /// Constructors can delegate to other constructors.
         #[ink(constructor)]
         pub fn default() -> Self {
-
-            Self::new(Default::default(), Default::default(), Default::default(), Default::default())
+            Self::new(
+                Default::default(),
+                Default::default(),
+                Default::default(),
+                Default::default(),
+            )
         }
 
         /// A message that can be called on instantiated contracts.
@@ -87,14 +95,14 @@ mod incrementer {
 
         /// Simply set the current value of our `number`.
         #[ink(message)]
-        pub fn get_number_lazy (&mut self) -> u32 {
+        pub fn get_number_lazy(&mut self) -> u32 {
             let x = ink_storage::Lazy::<u32>::get(&mut self.lazy_number);
             x.clone()
         }
 
         /// Simply set the current value of our `number`.
         #[ink(message)]
-        pub fn set_number_lazy (&mut self, new_value: u32) {
+        pub fn set_number_lazy(&mut self, new_value: u32) {
             ink_storage::Lazy::<u32>::set(&mut self.lazy_number, new_value);
         }
 
@@ -153,7 +161,13 @@ mod incrementer {
         /// We test a simple use case of our contract.
         #[ink::test]
         fn it_works() {
-            let mut incrementer = Incrementer::new(false, 4, Default::default(), Default::default(), Default::default());
+            let mut incrementer = Incrementer::new(
+                false,
+                4,
+                Default::default(),
+                Default::default(),
+                Default::default(),
+            );
             assert_eq!(incrementer.get_bool(), false);
             incrementer.flip();
             assert_eq!(incrementer.get_bool(), true);
@@ -162,7 +176,13 @@ mod incrementer {
         /// We test a simple use case of our contract.
         #[ink::test]
         fn number_test() {
-            let mut incrementer = Incrementer::new(false, 4, Default::default(), Default::default(), Default::default());
+            let mut incrementer = Incrementer::new(
+                false,
+                4,
+                Default::default(),
+                Default::default(),
+                Default::default(),
+            );
             assert_eq!(incrementer.get_number(), 4);
             incrementer.set_number(3);
             assert_eq!(incrementer.get_number(), 3);
@@ -177,7 +197,8 @@ mod incrementer {
         /// We test a simple use case of our contract.
         #[ink::test]
         fn lazy_number_test() {
-            let mut incrementer = Incrementer::new(false, 4, 4, Default::default(), Default::default());
+            let mut incrementer =
+                Incrementer::new(false, 4, 4, Default::default(), Default::default());
             assert_eq!(incrementer.get_number_lazy(), 4);
             incrementer.set_number_lazy(3);
             assert_eq!(incrementer.get_number_lazy(), 3);
@@ -188,6 +209,5 @@ mod incrementer {
             incrementer.inc_lazy(2);
             assert_eq!(incrementer.get_number_lazy(), 10);
         }
-
     }
 }
